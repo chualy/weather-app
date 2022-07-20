d62a21ae0781d498e8d8d1318d5dc02bbb525cee
chualy <107922628+chualy@users.noreply.github.com>
2022-07-21 01:36:19 +0800

    1. Updated CSS of main weather app. 2. Added JS with API URL.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+function displayTemp(response) {
+  console.log(response.data);
+  let currentTemp = Math.round(response.data.main.temp);
+  let cityTemp = document.querySelector("#temp-number");
+  cityTemp.innerHTML = `${currentTemp}`;
+}
 
+let apiKey = "166b9c0e1c83778d358b7f11ac37349b";
+let unit = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Singapore&units=${unit}&appid=${apiKey}`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemp);
