6188161aa046d4c7a3e7d08bbf12ed7751b8df9d
chualy <107922628+chualy@users.noreply.github.com>
2022-07-21 23:54:55 +0800

    Updated HTML, CSS, Javascript to include weather information from API.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 function displayTemp(response) {
   console.log(response.data);
-  let currentTemp = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
   let cityTemp = document.querySelector("#temp-number");
-  cityTemp.innerHTML = `${currentTemp}`;
+  cityTemp.innerHTML = Math.round(response.data.main.temp);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement = ((`(response.data.wind.speed)` * 60 * 60) / 1000).toFixed(1);
 }
 
 let apiKey = "166b9c0e1c83778d358b7f11ac37349b";
