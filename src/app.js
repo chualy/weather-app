d835e92f109d74963c723f6716f3c3dd8a9d66e1
chualy <107922628+chualy@users.noreply.github.com>
2022-07-22 00:37:59 +0800

    Update HTML and JS to add time function.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemp(response) {
   console.log(response.data);
 
@@ -15,6 +42,9 @@ function displayTemp(response) {
 
   let windElement = document.querySelector("#wind");
   windElement = ((`(response.data.wind.speed)` * 60 * 60) / 1000).toFixed(1);
+
+  let daytimeElement = document.querySelector("#day-time");
+  daytimeElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "166b9c0e1c83778d358b7f11ac37349b";
